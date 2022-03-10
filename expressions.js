dd89bab1c4eca08d5107d55e063d6e62b6daa4e4
TaylorMaDEWotK18 <84547566+TaylorMaDEWotK18@users.noreply.github.com>
2022-03-10 00:02:05 -0700

    added addition practice decs and exps

--- expressions.js
+++ expressions.js
@@ -1,17 +1,37 @@
+// //Function Declaration 
+
+// console.log(getRandomNumber(1000));
+
+// function getRandomNumber(upper) {
+//   const randomNumber = Math.floor(Math.random() * upper ) + 1;
+//   return randomNumber;
+// }
+
+// //Function Expression
+
+// const getRandomNumber = function(upper) {
+//   const randomNumber = Math.floor(Math.random() * upper ) + 1;
+//   return randomNumber;
+// }
+
+// console.log(getRandomNumber(1000));
+
 //Function Declaration 
 
-console.log(getRandomNumber(1000));
+// function sNES() {
+//   const superMario = 'LEGO!';
 
-function getRandomNumber(upper) {
-  const randomNumber = Math.floor(Math.random() * upper ) + 1;
-  return randomNumber;
-}
+//   alert(`Mario always loves to yell ${superMario} when he gets a good score!`);
+// }
+
+// sNES();
 
 //Function Expression
 
-const getRandomNumber = function(upper) {
-  const randomNumber = Math.floor(Math.random() * upper ) + 1;
-  return randomNumber;
+const wuigi = function() {
+  const oneUp = 'EXTRA LIFE!';
+
+  alert(`Finally finding one of those pesky green mushrooms gives you an ${oneUp}`);
 }
 
-console.log(getRandomNumber(1000));
+console.log(wuigi());
